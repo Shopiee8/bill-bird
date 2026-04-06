6dd22733d360b7ec03ca68b761f517c3c902785d
Shopiee8 <alldeales@outlook.com>
2026-04-06 19:33:55 +0400

    Initial commit: Bill Bird app with subscription management

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {Link} from "expo-router";
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+            <Link href="/(auth)/sign-up">Sign In</Link>
+        </View>
+    )
+}
+
+export default SignUp;
